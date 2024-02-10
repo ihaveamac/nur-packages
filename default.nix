d473a9b04307e7714bb3fed010eeadbb34aaf858
ihaveahax <ian@ianburgwin.net>
2024-02-09 20:58:53 -0600

    add 3dstool, set up github actions, update flake.lock

--- default.nix
+++ default.nix
@@ -15,6 +15,7 @@
   overlays = import ./overlays; # nixpkgs overlays
 
   example-package = pkgs.callPackage ./pkgs/example-package { };
+  "3dstool" = pkgs.callPackage ./pkgs/3dstool { };
   # some-qt5-package = pkgs.libsForQt5.callPackage ./pkgs/some-qt5-package { };
   # ...
 }
