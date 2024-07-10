54efadf397186f6bcc859b3a7552eba33b604752
ihaveahax <ian@ianburgwin.net>
2024-07-09 20:33:50 -0500

    update homebox to new repo, 0.12.1

--- README.md
+++ README.md
@@ -24,7 +24,7 @@ Attributes listed on [the NUR](https://nur.nix-community.org/repos/ihaveamac/)
 * mediawiki-1.40.4
 * mediawiki-1.41.2
 * mediawiki-1.42.1
-* homebox-bin-0.10.3
+* homebox-bin-0.12.1
 * 3dslink-0.6.1 (as \_3dslink attribute)
 * discordwikibot-2024-07-01
 
--- pkgs/homebox-bin/default.nix
+++ pkgs/homebox-bin/default.nix
@@ -2,7 +2,7 @@
 
 stdenvNoCC.mkDerivation rec {
   pname = "homebox-bin";
-  version = "0.10.3";
+  version = "0.12.1";
 
   src = passthru.sources.${stdenvNoCC.hostPlatform.system}
     or (throw "Unsupported system: ${stdenvNoCC.hostPlatform.system}");
@@ -10,29 +10,29 @@ stdenvNoCC.mkDerivation rec {
   passthru = {
     sources = rec {
       x86_64-linux = fetchzip {
-        url = "https://github.com/hay-kot/homebox/releases/download/v${version}/homebox_Linux_x86_64.tar.gz";
-        sha256 = "sha256-R1EmV4AJv+vEFn21cUsxSs40fvcK+wPGPwqqP9K6Aek=";
+        url = "https://github.com/sysadminsmedia/homebox/releases/download/v${version}/homebox_Linux_x86_64.tar.gz";
+        sha256 = "sha256-NUMUHw3Mi5v8hJuXRP6QQm/wU8p7wN9s1ImRBGwpVqo=";
       };
       i686-linux = fetchzip {
-        url = "https://github.com/hay-kot/homebox/releases/download/v${version}/homebox_Linux_i386.tar.gz";
-        sha256 = "sha256-T7XBw16/crJxk2EYcoRaPbbsq1v2l4O+b/HNw1p53bI=";
+        url = "https://github.com/sysadminsmedia/homebox/releases/download/v${version}/homebox_Linux_i386.tar.gz";
+        sha256 = "sha256-trpR7ukihsippS3DsW9blkXx3s9Y3XHqSw3X5iF3s/g=";
       };
       aarch64-linux = fetchzip {
-        url = "https://github.com/hay-kot/homebox/releases/download/v${version}/homebox_Linux_arm64.tar.gz";
-        sha256 = "sha256-5TII40HJkIItsB+VpF8tFLuI07SrmM1icPgwM0kiQtQ=";
+        url = "https://github.com/sysadminsmedia/homebox/releases/download/v${version}/homebox_Linux_arm64.tar.gz";
+        sha256 = "sha256-lmKbUQYw9IPa32Ol/xUDXHyEk+7MbwJVMwsRf2ZUDnE=";
       };
       armv6l-linux = fetchzip {
-        url = "https://github.com/hay-kot/homebox/releases/download/v${version}/homebox_Linux_arm64.tar.gz";
-        sha256 = "sha256-5TII40HJkIItsB+VpF8tFLuI07SrmM1icPgwM0kiQtQ=";
+        url = "https://github.com/sysadminsmedia/homebox/releases/download/v${version}/homebox_Linux_armv6.tar.gz";
+        sha256 = "sha256-dYIJqB+48QQM9lTwhDmW7yhngG5YF9hpLhDVysVYZZQ=";
       };
       armv7l-linux = armv6l-linux;
       x86_64-darwin = fetchzip {
-        url = "https://github.com/hay-kot/homebox/releases/download/v${version}/homebox_Darwin_x86_64.tar.gz";
-        sha256 = "sha256-QAOAR6eezhVVP5htRUzdZxa6wuxoR7+EAon0rSrWNJY=";
+        url = "https://github.com/sysadminsmedia/homebox/releases/download/v${version}/homebox_Darwin_x86_64.tar.gz";
+        sha256 = "sha256-lxpYFqNmRussLYB+mNLiIiKIr0Wsatk93IzsSr3TDbA=";
       };
       aarch64-darwin = fetchzip {
-        url = "https://github.com/hay-kot/homebox/releases/download/v${version}/homebox_Darwin_arm64.tar.gz";
-        sha256 = "sha256-JU10ijRozr8unm7jnkxF5dKu39st5/8RYCWdyt5RHQc=";
+        url = "https://github.com/sysadminsmedia/homebox/releases/download/v${version}/homebox_Darwin_arm64.tar.gz";
+        sha256 = "sha256-ITWbj1tZKFF3KiCtitCM9xiRMh/iAj2rUME0oAkg3WM=";
       };
     };
   };
