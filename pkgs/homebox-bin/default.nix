211070129f724ef3396722e5808b34f3723bd3a5
ihaveahax <ian@ianburgwin.net>
2024-07-27 17:45:26 -0500

    update homebox to 0.13.0

--- pkgs/homebox-bin/default.nix
+++ pkgs/homebox-bin/default.nix
@@ -2,7 +2,7 @@
 
 stdenvNoCC.mkDerivation rec {
   pname = "homebox-bin";
-  version = "0.12.1";
+  version = "0.13.0";
 
   src = passthru.sources.${stdenvNoCC.hostPlatform.system}
     or (throw "Unsupported system: ${stdenvNoCC.hostPlatform.system}");
@@ -11,28 +11,28 @@ stdenvNoCC.mkDerivation rec {
     sources = rec {
       x86_64-linux = fetchzip {
         url = "https://github.com/sysadminsmedia/homebox/releases/download/v${version}/homebox_Linux_x86_64.tar.gz";
-        sha256 = "sha256-NUMUHw3Mi5v8hJuXRP6QQm/wU8p7wN9s1ImRBGwpVqo=";
+        sha256 = "sha256-vsgvqhHGwzOqsh/Rf1pPwU7zTUb5lfRujUqfBRHf0Ng=";
       };
       i686-linux = fetchzip {
         url = "https://github.com/sysadminsmedia/homebox/releases/download/v${version}/homebox_Linux_i386.tar.gz";
-        sha256 = "sha256-trpR7ukihsippS3DsW9blkXx3s9Y3XHqSw3X5iF3s/g=";
+        sha256 = "sha256-K9N8ychmCRuktiG0yte+auZdWsWCQptBCz9axnMzlpA=";
       };
       aarch64-linux = fetchzip {
         url = "https://github.com/sysadminsmedia/homebox/releases/download/v${version}/homebox_Linux_arm64.tar.gz";
-        sha256 = "sha256-lmKbUQYw9IPa32Ol/xUDXHyEk+7MbwJVMwsRf2ZUDnE=";
+        sha256 = "sha256-KgUS+p49+EcE8S5la0o2ybFFy8u7hOxB0/lgpvSdfQE=";
       };
       armv6l-linux = fetchzip {
         url = "https://github.com/sysadminsmedia/homebox/releases/download/v${version}/homebox_Linux_armv6.tar.gz";
-        sha256 = "sha256-dYIJqB+48QQM9lTwhDmW7yhngG5YF9hpLhDVysVYZZQ=";
+        sha256 = "sha256-RpSY+MDdn8lmgikfIzSi0daUa48tEa6cCN27POnXB6k=";
       };
       armv7l-linux = armv6l-linux;
       x86_64-darwin = fetchzip {
         url = "https://github.com/sysadminsmedia/homebox/releases/download/v${version}/homebox_Darwin_x86_64.tar.gz";
-        sha256 = "sha256-lxpYFqNmRussLYB+mNLiIiKIr0Wsatk93IzsSr3TDbA=";
+        sha256 = "sha256-tqKlpDPGJr3+4+85WdQi14NXAxOUgVjd04L4ZZw2b78=";
       };
       aarch64-darwin = fetchzip {
         url = "https://github.com/sysadminsmedia/homebox/releases/download/v${version}/homebox_Darwin_arm64.tar.gz";
-        sha256 = "sha256-ITWbj1tZKFF3KiCtitCM9xiRMh/iAj2rUME0oAkg3WM=";
+        sha256 = "sha256-BNw/v0w2tbi2Y099ONYg/TMXNOdCXIpcjDrGM0aWubg=";
       };
     };
   };
