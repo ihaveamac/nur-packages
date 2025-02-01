30c1c0eaa1388e87358bd212f9a0863c6c37176c
ihaveahax <ian@ianburgwin.net>
2025-02-01 11:52:14 -0600

    rofs-dumper: force use newer gcc/clang (mainly for 24.11)

--- pkgs/rofs-dumper/default.nix
+++ pkgs/rofs-dumper/default.nix
@@ -1,6 +1,9 @@
-{ lib, stdenv, fetchFromGitHub }:
+{ lib, stdenv, clang19Stdenv, gcc14Stdenv, fetchFromGitHub }:
 
-stdenv.mkDerivation rec {
+let
+  realStdenv = if stdenv.cc.isClang then clang19Stdenv else gcc14Stdenv;
+in
+realStdenv.mkDerivation rec {
   pname = "rofs-dumper";
   version = "1.0";
 
@@ -12,7 +15,7 @@ stdenv.mkDerivation rec {
   };
 
   buildPhase = ''
-    ${stdenv.cc.targetPrefix}c++ -std=c++20 main.cpp -o rofs_dump
+    ${realStdenv.cc.targetPrefix}c++ -std=c++20 main.cpp -o rofs_dump
   '';
 
   installPhase = ''
