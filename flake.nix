f2ba6da4d0c2349f8feabad8359cb7fccde134d9
ihaveahax <ian@ianburgwin.net>
2025-02-02 11:27:14 -0600

    add formatter to flake, using treefmt-nix

--- flake.nix
+++ flake.nix
@@ -1,8 +1,14 @@
 {
   description = "My personal NUR repository";
   inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+  inputs.treefmt-nix.url = "github:numtide/treefmt-nix";
+  inputs.treefmt-nix.inputs.nixpkgs.follows = "nixpkgs";
   outputs =
-    { self, nixpkgs }:
+    {
+      self,
+      nixpkgs,
+      treefmt-nix,
+    }:
     let
       systems = [
         "x86_64-linux"
@@ -14,6 +20,13 @@
         "armv7l-linux"
       ];
       forAllSystems = f: nixpkgs.lib.genAttrs systems (system: f system);
+      treefmtEval = forAllSystems (
+        system:
+        let
+          pkgs = import nixpkgs { inherit system; };
+        in
+        treefmt-nix.lib.evalModule pkgs ./treefmt.nix
+      );
     in
     {
       legacyPackages = forAllSystems (
@@ -32,5 +45,9 @@
       hmModules = {
         lnshot = import ./hm-modules/lnshot.nix;
       };
+      formatter = forAllSystems (system: treefmtEval.${system}.config.build.wrapper);
+      checks = forAllSystems (system: {
+        formatting = treefmtEval.${system}.config.build.check self;
+      });
     };
 }
