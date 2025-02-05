627fdafc2bb2f38c4096b7bc384e31f56f1f5486
ihaveahax <ian@ianburgwin.net>
2025-02-05 00:58:50 -0600

    add 3beans

---
+++ pkgs/3beans/default.nix
@@ -0,0 +1,30 @@
+{ lib, gccStdenv, fetchFromGitHub, pkg-config, wxGTK32, portaudio, libGL }:
+
+let
+  stdenv = gccStdenv;
+in
+stdenv.mkDerivation rec {
+  name = "3beans";
+  ver = "unstable";
+
+  src = fetchFromGitHub {
+    owner = "Hydr8gon";
+    repo = "3Beans";
+    rev = "f65d0fa078f561050b3a76ac8771f676e8ec2022";
+    hash = "sha256-RzJSZJhYjYxT07TzqKIRUqda7ExAKqGPSoqhg8aKiOw=";
+  };
+
+  buildInputs = [ wxGTK32 portaudio libGL ];
+  nativeBuildInputs = [ pkg-config ];
+
+  makeFlags = [ "DESTDIR=${placeholder "out"}" ];
+
+  meta = with lib; {
+    license = licenses.gpl3;
+    description = "A low-level 3DS emulator (maybe)";
+    homepage = "https://github.com/Hydr8gon/3Beans";
+    platforms = platforms.all;
+    broken = stdenv.isDarwin;
+    mainProgram = "3beans";
+  };
+}
