6921101246d19227160a22d48f2c122d1ebede16
ihaveahax <ian@ianburgwin.net>
2025-08-20 17:18:11 -0500

    tiny-wii-backup-manager: set up desktop file and icons for linux + app bundle for mac

--- pkgs/tiny-wii-backup-manager/default.nix
+++ pkgs/tiny-wii-backup-manager/default.nix
@@ -1,9 +1,23 @@
 {
   lib,
+  stdenv,
   rustPlatform,
+  makeDesktopItem,
   fetchFromGitHub,
 }:
 
+let
+  name = "tiny-wii-backup-manager";
+  # based on https://github.com/mq1/TinyWiiBackupManager/blob/main/scripts/bundle.py
+  # maybe this should read it from Cargo.toml, like the script does
+  desktopItem = makeDesktopItem {
+    inherit name;
+    exec = name;
+    desktopName = "TinyWiiBackupManager";
+    comment = "A dead simple Wii Backup Manager";
+    categories = [ "Utility" ];
+  };
+in
 rustPlatform.buildRustPackage rec {
   pname = "TinyWiiBackupManager";
   version = "1.3.5";
@@ -17,11 +31,29 @@ rustPlatform.buildRustPackage rec {
 
   cargoHash = "sha256-SWKASbtDAgK72Ocodyy6YjGnUrfIHFvBHx/aSgWIDmY=";
 
+  postInstall =
+    if stdenv.isDarwin then
+      ''
+        appContents=$out/Applications/TinyWiiBackupManager.app/Contents
+        mkdir -p $appContents/MacOS $appContents/Resources
+        ln -s $out/bin/${name} $appContents/MacOS/
+        cp ${./Info.plist} $appContents/Info.plist
+        cp ${src}/assets/macos/${name}.icns $appContents/Resources
+        substituteInPlace $appContents/Info.plist --replace-fail VERSION ${version}
+      ''
+    else
+      ''
+        mkdir -p $out/share/applications
+        cp ${desktopItem}/share/applications/*.desktop $out/share/applications
+        mkdir -p $out/share/icons/hicolor
+        cp -r ${src}/assets/linux/* $out/share/icons/hicolor
+      '';
+
   meta = with lib; {
     description = "Tool for decrypting all mogg files used by the Rock Band series";
     homepage = "https://github.com/mq1/TinyWiiBackupManager";
     license = licenses.gpl2Only;
     platforms = platforms.all;
-    mainProgram = "tiny-wii-backup-manager";
+    mainProgram = name;
   };
 }
