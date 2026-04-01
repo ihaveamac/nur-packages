cd1a0167cf15e5db24dab74efbd9fa91d553358f
ihaveahax <ian@ianburgwin.net>
2026-03-31 23:21:16 -0500

    azahar-master: update

--- pkgs/azahar-master/package.nix
+++ pkgs/azahar-master/package.nix
@@ -2,12 +2,12 @@
 
 azahar.overrideAttrs (final: prev: {
   pname = "azahar";
-  version = "2125.0-rc4-unstable-2026-03-22";
+  version = "2125.0.1-unstable-2026-03-29";
   src = fetchFromGitHub {
     owner = "azahar-emu";
     repo = "azahar";
-    rev = "7a600e28d2807fc2b07fe7be7eb84952764bbfd3";
-    hash = "sha256-OQgOWcZvJU1Id2mWTUz6Wc2wMesit6+xKZScRfnAHQ8=";
+    rev = "3066887ff4cf9f89e626047d0f142ad231053cf1";
+    hash = "sha256-xfFwMcxWjED9gVrejXcNIMYQdfG3qwKWUoL80bRrdCg=";
     fetchSubmodules = true;
   };
 
