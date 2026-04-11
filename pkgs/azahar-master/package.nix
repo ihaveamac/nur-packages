4ccee4d0842d31ad5c5e354a203d47263c87b55d
ihaveahax <ian@ianburgwin.net>
2026-04-10 22:04:21 -0500

    azahar-master: update

--- pkgs/azahar-master/package.nix
+++ pkgs/azahar-master/package.nix
@@ -2,14 +2,18 @@
 
 azahar.overrideAttrs (final: prev: {
   pname = "azahar";
-  version = "2125.0.1-unstable-2026-04-05";
+  version = "2125.0.1-unstable-2026-04-09";
   src = fetchFromGitHub {
     owner = "azahar-emu";
     repo = "azahar";
-    rev = "ba6f8cb744e59deaaaefbb46b532b405b09112ac";
-    hash = "sha256-K4NPkRotoKpuZ084ggtp4zDQSUHoDZE+K3KNNNZ6o9I=";
+    rev = "b2faa299d5890a65ff979fcb379d2b20d0ca36fc";
+    hash = "sha256-ctdU5NvIDIVPaQEaDADZfipMKCP1pxqPMlNCikKhhAM=";
     fetchSubmodules = true;
   };
+
+  # remove unnecessary patch
+  # TODO: remove this removal once nixpkgs has caught up
+  patches = [];
 
   meta = prev.meta // {
     description = prev.meta.description + " (master branch)";
